f8ac4ed91842214fae4f7f9e2d15788ac7b909fd
Bas <bysebashdez@gmail.com>
2023-10-30 21:57:47 -0500

    fix show error

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def show():
     return render_template("newcard.html")
 
 
-@app.route('/api/card/', methods=['POST'])
+@app.route('/api/card/')
 def showpass():
     return render_template("pass.html")
 
@@ -32,18 +32,18 @@ R1
 """
 
 
-@app.route('/api/card/new', methods=['POST'])
+@app.route('/api/card/new')
 def createcard():
     try:
-        card_number = request.form["card_number"]
-        owner_id = request.form["owner_id"]
-        owner_name = request.form["owner_name"]
-        bank_name = request.form["bank_name"]
-        due_date = request.form["due_date"]
-        franchise = request.form["franchise"]
-        payment_day = int(request.form["payment_day"])
-        monthly_fee = int(request.form["monthly_fee"])
-        interest_rate = float(request.form["interest_rate"])
+        card_number = request.args["card_number"]
+        owner_id = request.args["owner_id"]
+        owner_name = request.args["owner_name"]
+        bank_name = request.args["bank_name"]
+        due_date = request.args["due_date"]
+        franchise = request.args["franchise"]
+        payment_day = int(request.args["payment_day"])
+        monthly_fee = int(request.args["monthly_fee"])
+        interest_rate = float(request.args["interest_rate"])
 
         creditcard_test = CreditCard(card_number, owner_id, owner_name, bank_name, due_date, franchise, payment_day,
                                      monthly_fee, interest_rate)
